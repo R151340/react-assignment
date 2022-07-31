a2d5792f9d6592f2ca3914cd2959066302b3d161
hassanulinam <hassanulinam143@gmail.com>
2022-07-31 06:16:18 +0530

    Added CustomPopup to ResourceCard with some fake-data genereted by *fakerjs*

---
+++ src/components/CustomPopup/index.tsx
@@ -0,0 +1,39 @@
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import { faker } from "@faker-js/faker";
+import { ResourceType } from "../../config/customTypes";
+import "./index.css";
+
+type CustomProps = {
+  children: JSX.Element;
+  data: ResourceType;
+};
+
+export default ({ children, data }: CustomProps) => (
+  <Popup
+    trigger={children}
+    position="top center"
+    modal={true}
+    className="popup-content"
+  >
+    <div className="popup-modal-bgcontainer">
+      <img
+        alt="random-image"
+        src={data.icon_url}
+        className="modal-content-photo"
+      />
+      <h1 className="content-title">{data.title}</h1>
+      <p>{faker.lorem.paragraphs(5, "\n")}</p>
+      <br />
+      <p>{faker.lorem.paragraphs(3, "\n")}</p>
+      <br />
+      <p>{faker.lorem.paragraphs(4, "\n")}</p>
+      <br />
+      <p>{faker.lorem.paragraphs(3, "\n")}</p>
+      <br />
+      <p>{faker.lorem.paragraphs(2, "\n")}</p>
+      <br />
+      <p>{faker.lorem.paragraphs(4, "\n")}</p>
+    </div>
+  </Popup>
+);
--- src/components/ResourceCard/index.tsx
+++ src/components/ResourceCard/index.tsx
@@ -1,4 +1,5 @@
 import { ResourceType } from "../../config/customTypes";
+import CustomPopup from "../CustomPopup";
 import {
   CardBgContainer,
   IconTitleRow,
@@ -13,21 +14,23 @@ type CustomProps = { data: ResourceType };
 
 const ResourceCard = ({ data }: CustomProps) => {
   return (
-    <CardBgContainer>
-      <IconTitleRow>
-        <IconContainer>
-          <Icon alt="source-logo" src={data.icon_url} />
-        </IconContainer>
-        <div>
-          <Title>{data.title}</Title>
-          <Subtitle>{data.category}</Subtitle>
-        </div>
-      </IconTitleRow>
-      <Description as={"a"} href={data.link}>
-        {data.link}
-      </Description>
-      <Description>{data.description}</Description>
-    </CardBgContainer>
+    <CustomPopup data={data}>
+      <CardBgContainer>
+        <IconTitleRow>
+          <IconContainer>
+            <Icon alt="source-logo" src={data.icon_url} />
+          </IconContainer>
+          <div>
+            <Title>{data.title}</Title>
+            <Subtitle>{data.category}</Subtitle>
+          </div>
+        </IconTitleRow>
+        <Description as={"a"} href={data.link}>
+          {data.link}
+        </Description>
+        <Description>{data.description}</Description>
+      </CardBgContainer>
+    </CustomPopup>
   );
 };
 
